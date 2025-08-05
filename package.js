da1c6f6d9fc9742d7dad3cd5aa544b5844222067
Jaco Swarts <jaco@vatfree.com>
2025-08-05 20:29:33 +0200

    Version bump

--- package.js
+++ package.js
@@ -1,47 +1,45 @@
 Package.describe({
-  name: 'gfk:underscore-deep',
-  version: '0.1.0',
-  summary: '',
-  git: 'git@github.com:gfk-ba/underscore.deep.git',
-  documentation: 'README.md'
+  name: "gfk:underscore-deep",
+  version: "0.1.1",
+  summary: "",
+  git: "git@github.com:gfk-ba/underscore.deep.git",
+  documentation: "README.md",
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
+Package.onUse(function (api) {
+  api.versionsFrom(["2"]);
+  api.use(["underscore", "coffeescript@2.4.1"]);
+
+  api.addFiles("underscore.deep.coffee");
+  api.export("_");
+});
+
+Package.onTest(function (api) {
   api.use([
-      'underscore',
-      'coffeescript'
+    "underscore",
+    "coffeescript",
+    "gfk:underscore-deep",
+    "mike:mocha-package@0.5.8",
+    "practicalmeteor:sinon",
+    "practicalmeteor:chai",
   ]);
 
-  api.addFiles('underscore.deep.coffee');
-  api.export('_');
-});
-
-Package.onTest(function(api) {
-  api.use([
-    'underscore',
-    'coffeescript',
-    'gfk:underscore-deep',
-    'mike:mocha-package@0.5.8',
-    'practicalmeteor:sinon',
-    'practicalmeteor:chai']);
-
   api.addFiles([
-      'test/inspect.js',
-      'test/helpers.coffee',
-      'test/deepClone.coffee',
-      'test/deepDelete.coffee',
-      'test/deepExtend.coffee',
-      'test/deepFromFlat.coffee',
-      'test/deepHas.coffee',
-      'test/deepKeys.coffee',
-      'test/deepMapValues.coffee',
-      'test/deepOmit.coffee',
-      'test/deepPick.coffee',
-      'test/deepToFlat.coffee',
-      'test/isPlainObject.coffee',
-      'test/mapKeys.coffee',
-      'test/mapValues.coffee',
-      'test/deepPickValue.coffee'
+    "test/inspect.js",
+    "test/helpers.coffee",
+    "test/deepClone.coffee",
+    "test/deepDelete.coffee",
+    "test/deepExtend.coffee",
+    "test/deepFromFlat.coffee",
+    "test/deepHas.coffee",
+    "test/deepKeys.coffee",
+    "test/deepMapValues.coffee",
+    "test/deepOmit.coffee",
+    "test/deepPick.coffee",
+    "test/deepToFlat.coffee",
+    "test/isPlainObject.coffee",
+    "test/mapKeys.coffee",
+    "test/mapValues.coffee",
+    "test/deepPickValue.coffee",
   ]);
 });
